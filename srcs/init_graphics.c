ff1ba405fb77bef6a91a19d35aae7e5a5fbc43c3
elpapi <andrecanelaskaldeich@gmail.com>
2023-11-07 05:46:56 +0000

    text init

--- srcs/init_graphics.c
+++ srcs/init_graphics.c
@@ -6,7 +6,7 @@
 /*   By: acanelas <acanelas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 03:11:21 by acanelas          #+#    #+#             */
-/*   Updated: 2023/11/03 05:21:14 by acanelas         ###   ########.fr       */
+/*   Updated: 2023/11/07 04:55:39 by acanelas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,25 @@
 
 void	check_img(t_game *game)
 {
-	if (!game->animation.noth_img || !game->animation.east_img
-	|| !game->animation.west_img || !game->animation.south_img)
+	if (!game->noth_img.img || !game->east_img.img
+	|| !game->west_img.img || !game->south_img.img)
 		exit_error(game, "convert xmp to img just failed\n");
+}
+
+void	init_addr(t_game *game)
+{
+	game->noth_img.addr = mlx_get_data_addr(game->noth_img.img, 
+		&game->noth_img.bpp, &game->noth_img.line_len, 
+		&game->noth_img.endian);
+	game->east_img.addr = mlx_get_data_addr(game->east_img.img, 
+		&game->east_img.bpp, &game->east_img.line_len, 
+		&game->east_img.endian);
+	game->west_img.addr = mlx_get_data_addr(game->west_img.img, 
+		&game->west_img.bpp, &game->west_img.line_len, 
+		&game->west_img.endian);
+	game->south_img.addr = mlx_get_data_addr(game->south_img.img, 
+		&game->south_img.bpp, &game->south_img.line_len, 
+		&game->south_img.endian);
 }
 
 void	init_xpm_img(t_game *game)
@@ -24,11 +40,12 @@ void	init_xpm_img(t_game *game)
 	int	tile_size;
 
 	tile_size = TILE_SIZE;
-	game->animation.noth_img = mlx_xpm_file_to_image(game->mlx, NORTH_IMG, &tile_size, &tile_size);
-	game->animation.south_img = mlx_xpm_file_to_image(game->mlx, SOUTH_IMG, &tile_size, &tile_size);
-	game->animation.east_img = mlx_xpm_file_to_image(game->mlx, EAST_IMG, &tile_size, &tile_size);
-	game->animation.west_img = mlx_xpm_file_to_image(game->mlx, WEST_IMG, &tile_size, &tile_size);
+	game->noth_img.img = mlx_xpm_file_to_image(game->mlx, NORTH_IMG, &tile_size, &tile_size);
+	game->south_img.img = mlx_xpm_file_to_image(game->mlx, SOUTH_IMG, &tile_size, &tile_size);
+	game->east_img.img = mlx_xpm_file_to_image(game->mlx, EAST_IMG, &tile_size, &tile_size);
+	game->west_img.img = mlx_xpm_file_to_image(game->mlx, WEST_IMG, &tile_size, &tile_size);
 	check_img(game);
+	init_addr(game);
 }
 
 void	init_graphics(t_game *game)
@@ -36,6 +53,4 @@ void	init_graphics(t_game *game)
 	game->mlx = mlx_init();
 	game->mlx_window = mlx_new_window(game->mlx, W_WIDTH, W_HEIGHT, "cub3d");
 	init_xpm_img(game);
-	//printf("floor = %d\n", game->floor);
-	//printf("ceiling = %d\n", game->ceiling);
 }
